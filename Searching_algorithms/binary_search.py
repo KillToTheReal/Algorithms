72a602a7d2d81bc616d6ec581d8ef140983c8655
KillToTheReal <73242190+KillToTheReal@users.noreply.github.com>
2021-06-08 22:03:38 +0300

    Create binary_search.py

---
+++ Searching_algorithms/binary_search.py
@@ -0,0 +1,28 @@
+'''
+
+В бинарный поиск поступает отсортированный массив, делим его пополам и спрашиваем:
+Больше или меньше искомого? Исходя из этого отсеиваем пол-массива.
+
+'''
+
+def binary_search_iterative(array, element):
+    mid = 0
+    start = 0
+    end = len(array)
+    step = 0
+
+    while (start <= end):
+        print("Subarray in step {}: {}".format(step, str(array[start:end+1])))
+        step = step+1
+        mid = (start + end) // 2
+
+        if element == array[mid]:
+            return mid
+
+        if element < array[mid]:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return -1
+    
+binary_search_iterative([2,4,5,6,7,8,9,20,21,25,54,56,65,76,78,96],96)
